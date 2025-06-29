6a79d4ca9ad0a2a01d115561140bf415e18c87be
Aditya-Developer-2007 <aaditya.saini2602@gmail.com>
2025-06-29 12:58:20 +0530

    u

--- first.py
+++ first.py
@@ -1,17 +1,87 @@
-a = 2
-b = 5
-# here a and b are two variables.
+# a = 2
+# b = 5
+# # here a and b are two variables.
 
-# Arithematic Operators
-# Add (+) 
-print("a + b =", a + b)
-# Subtract (-)
-print("a - b =", a - b)
-# Multiply (*)
-print("a * b =", a * b)
-# Divide (/)
-print("a / b =", a / b)
-# Modulus (%)
-print("a % b =", a % b)
-# Exponentiation (**)
-print("a ** b =", a ** b)
+# # Arithematic Operators
+# # Add (+) 
+# print("a + b =", a + b) #7
+# # Subtract (-)
+# print("a - b =", a - b) #-3
+# # Multiply (*)
+# print("a * b =", a * b) #10
+# # Divide (/)
+# print("a / b =", a / b) #0.4
+# # Modulus (%)
+# print("a % b =", a % b) #2
+# # Exponentiation (**)
+# print("a ** b =", a ** b) #32
+
+
+# # Comparison Operators
+# # Equal to (==)
+# print("a == b:", a == b) #False
+# # Not equal to (!=)
+# print("a != b:", a != b) #True
+# # Greater than (>)  
+# print("a > b:", a > b) #False
+# # Less than (<)
+# print("a < b:", a < b) #True
+# # Greater than equal to (>=)
+# print("a >= b:", a >= b) #False
+# # Less than equal to (<=)
+# print("a <= b:", a <= b) #True
+
+# #Assignment Operators
+# # Assign (=)
+# x = 10
+# print("x =", x) #10
+# # Add and assign (+=)
+# x += 5
+# print("x :", x) #15
+# # Subtract and assign (-=)
+# x -= 3
+# print("x :", x) #12
+# # Multiply and assign (*=)
+# x *= 2
+# print("x :", x) #24
+# # Divide and assign (/=)
+# x /= 4
+# print("x :", x) #6.0
+# # Modulus and assign (%=)
+# x %= 5
+# print("x :", x) #1.0
+# # Exponentiation and assign (**=)
+# x **= 3
+# print("x :", x) #1.0
+
+# # Logical Operators
+# # Logical AND (and)
+# p = True
+# q = False
+# print("p and q:", p and q) #False
+# # Logical OR (or)
+# print("p or q:", p or q) #True
+# # Logical NOT (not)
+# print("not p:", not p) #False
+
+
+
+
+
+# Q2 write a program to input two numbers and print their sum.
+# first = int(input("Enter first number: "))
+# second = int(input("Enter second number: "))
+# sum = first + second
+# print(sum)
+
+
+# Q3 write a program to input side of a square and print its area.
+# a = float(input("Enter side of square: "))
+# area = a * a
+# print("Area of square:", area)
+
+# Q4 write a program to input two floating point numbers and print their average.
+# a = float(input("enter 1st decimal number :"))
+# b = float(input("enter 2nd decimal number :"))
+# average = (a + b) / 2
+# print("Average of the two numbers is:", average)
