5dadd05e87617db1b75b452d530c93b25bc1ceb8
Aditya-Developer-2007 <aaditya.saini2602@gmail.com>
2025-06-29 11:42:55 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+a = 2
+b = 5
+# here a and b are two variables.
+
+# Arithematic Operators
+# Add (+) 
+print("a + b =", a + b)
+# Subtract (-)
+print("a - b =", a - b)
+# Multiply (*)
+print("a * b =", a * b)
+# Divide (/)
+print("a / b =", a / b)
+# Modulus (%)
+print("a % b =", a % b)
+# Exponentiation (**)
+print("a ** b =", a ** b)
