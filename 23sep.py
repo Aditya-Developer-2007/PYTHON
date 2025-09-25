dc0c23abc6912555744eb1bda26ac90b4a55dbcd
Aditya-Developer-2007 <aaditya.saini2602@gmail.com>
2025-09-25 11:01:36 +0530

    dd

---
+++ 23sep.py
@@ -0,0 +1,78 @@
+# # WAP to input three marks and calculate avg and pass or not with grade ?
+# m1 = int(input("Enter mark 1: "))
+# m2 = int(input("Enter mark 2: "))
+# m3 = int(input("Enter mark 3: "))
+# avg = (m1 + m2 + m3) / 3
+# if avg >= 40:
+#     print("Pass")
+# if 100 >= avg >= 90:
+#     print("A")
+# elif 89 >= avg >= 75:
+#     print("B")
+# elif 74 >= avg >=60:
+#     print("C")
+# elif 59 >= avg >= 40:
+#     print("D")
+# else:
+#     print("Fail")
+
+# a = "shenanigan"
+# print(a[0],a[1])
+# print(a[4],a[5])
+# print(a[2:])
+# print(a[:6])
+# print(a[:-4])
+# print(a[:6])
+# print(a[:6])
+# print(a)
+# print(a[::2])
+# print(a[::3])
+# print(a[::4])
+# print(a+"Type")
+# print(a[:6]+"Wabbite")
+
+# # cost price and selling price and calculate pnl
+# cprice = int(input("Enter cost price: "))
+# sprice = int(input("Enter selling price: "))
+# if sprice > cprice:
+#     print("profit%=", ((sprice - cprice) / cprice) * 100)
+# elif cprice > sprice:
+#     print("loss%=", ((cprice - sprice) / cprice) * 100)    
+# else:
+#     print("no profit no loss")
+
+
+# a = input("Enter something: ")
+# if a.islower():
+#     print(a,"is Lowercase")
+# elif a.isupper():
+#     print(a,"is Uppercase")
+# elif a.isdigit():
+#     print(a,"is Digit")
+# else:
+#     print(a,"is Special Character")  
+
+
+# vovel or consonent
+# a = input("Enter something: ")
+# if a in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+#     print(a, "is Vovel")
+# else:
+#     print(a, "is Consonent")   
+
+# week = int(input("Enter week number (1-7): "))
+
+# if week == 1:
+#     print("Monday")
+# elif week == 2:
+#     print("Tuesday")
+# elif week == 3:
+#     print("Wednesday")
+# elif week == 4:
+#     print("Thursday")
+# elif week == 5:
+#     print("Friday")
+# elif week == 6:
+#     print("Saturday")
+# elif week == 7:
+#     print("Sunday")    
--- 9sept.py
+++ 9sept.py
@@ -1,11 +1,11 @@
 
-a = "hello world "
-b = "abcdefghij "
-c = "uhdsfhjsdjh"
-d = 10
-e = 20
+# a = "hello world"
+# b = "abcdefghij "
+# c = "uhdsfhjsdjh"
+# d = 10
+# e = 20
 
-#slicing
+# #slicing
 # print(a[2:5])  
 # print(a[0:5])  
 # print(a[:5])   
@@ -15,101 +15,104 @@ e = 20
 # print(a[:-2])   
 # print(a[:])     
 # print(a[-5:-2]) 
-# print(a[-5:])    
+# print(a[-5:]) 
+# print(a[2:9:2])
+# print(a[::-1])
+# print(a[::-2])
 
-#methods
-print(a.replace("l", "j"))
-print(a.split("o"))
-print(a.upper())
-print(a.lower())
-print(a.capitalize())
-print(len(a))
+# #methods
+# print(a.replace("l", "j"))
+# print(a.split("o"))
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(len(a))
 
-#concatenation
-print("b + c =", b + c)
+# #concatenation
+# print("b + c =", b + c)
   
-# format method 
-# it formats the specify values and insert them inside the string's placeholder.
-#placeholder can be identified using index number, index name, or empty.
-print("the {} {} {}".format(a, b, c))
-print("the {2} {1} {0}".format(a, b, c))
-print("the {x} {y} {z}".format(x=a, y=b, z=c))
+# # format method 
+# # it formats the specify values and insert them inside the string's placeholder.
+# #placeholder can be identified using index number, index name, or empty.
+# print("the {} {} {}".format(a, b, c))
+# print("the {2} {1} {0}".format(a, b, c))
+# print("the {x} {y} {z}".format(x=a, y=b, z=c))
 
-# f string
-print(f"the {a} {b} {c}")
-print(f"the {a} {b} {c} {d} {e} {d+e}")
-print(f"the {a} {b} {c} {d} {e} {d*e}")
-
-
-# operators
-a = 2
-b = 5
+# # f string
+# print(f"the {a} {b} {c}")
+# print(f"the {a} {b} {c} {d} {e} {d+e}")
+# print(f"the {a} {b} {c} {d} {e} {d*e}")
 
 
-# Arithematic Operators 
-print("a + b =", a + b)
-print("a - b =", a - b) 
-print("a * b =", a * b)
-print("a / b =", a / b)
-print("a % b =", a % b)
-print("a ** b =", a ** b)
+# # operators
+# a = 2
+# b = 5
 
 
-# Comparison Operators
-print("a == b:", a == b) 
-print("a != b:", a != b) 
-print("a > b:", a > b)
-print("a < b:", a < b)
-print("a >= b:", a >= b) 
-print("a <= b:", a <= b) 
+# # Arithematic Operators 
+# print("a + b =", a + b)
+# print("a - b =", a - b) 
+# print("a * b =", a * b)
+# print("a / b =", a / b)
+# print("a % b =", a % b)
+# print("a ** b =", a ** b)
 
-#Assignment Operators
-x = 10
-print("x =", x)
-x += 5
-print("x :", x)
-x -= 3
-print("x :", x)
-x *= 2
-print("x :", x)
-x /= 4
-print("x :", x) 
-x %= 5
-print("x :", x)
-x **= 3
-print("x :", x) 
 
-# Logical Operators
-p = True
-q = False
-print("p and q:", p and q)
-print("p or q:", p or q) 
-print("not p:", not p) 
-print("not q:", not q) 
+# # Comparison Operators
+# print("a == b:", a == b) 
+# print("a != b:", a != b) 
+# print("a > b:", a > b)
+# print("a < b:", a < b)
+# print("a >= b:", a >= b) 
+# print("a <= b:", a <= b) 
 
-#Bitwise Operators
-print("a & b =", a & b)
-print("a | b =", a | b)
-print("~a =", ~a)
-print("a ^ b =", a ^ b)
-print("a >> 1 =", a >> 1) 
-print("a << 1 =", a << 1) 
+# #Assignment Operators
+# x = 10
+# print("x =", x)
+# x += 5
+# print("x :", x)
+# x -= 3
+# print("x :", x)
+# x *= 2
+# print("x :", x)
+# x /= 4
+# print("x :", x) 
+# x %= 5
+# print("x :", x)
+# x **= 3
+# print("x :", x) 
 
-# membership operators
-a = "hello world "
-b = "abcdefghij "
-c = "uhdsfhjsdjh"
-print("'h' in a:", 'h' in a) #True
-print("'z' in a:", 'z' in a) #False
-print("'h' not in a:", 'h' not in a) #False
-print("'z' not in a:", 'z' not in a) #True
+# # Logical Operators
+# p = True
+# q = False
+# print("p and q:", p and q)
+# print("p or q:", p or q) 
+# print("not p:", not p) 
+# print("not q:", not q) 
 
-# Identity operators
-m = 5
-n = 5
-print("m is n:", m is n) #True
-n = 10
-print("m is not n:", m is not n) #True
+# #Bitwise Operators
+# print("a & b =", a & b)
+# print("a | b =", a | b)
+# print("~a =", ~a)
+# print("a ^ b =", a ^ b)
+# print("a >> 1 =", a >> 1) 
+# print("a << 1 =", a << 1) 
+
+# # membership operators
+# a = "hello world "
+# b = "abcdefghij "
+# c = "uhdsfhjsdjh"
+# print("'h' in a:", 'h' in a) #True
+# print("'z' in a:", 'z' in a) #False
+# print("'h' not in a:", 'h' not in a) #False
+# print("'z' not in a:", 'z' not in a) #True
+
+# # Identity operators
+# m = 5
+# n = 5
+# print("m is n:", m is n) #True
+# n = 10
+# print("m is not n:", m is not n) #True
 
 
 
